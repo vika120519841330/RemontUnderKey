6e9b610a6f3efe3f7db9ba6a8d4515ec02edea81
Vika Strigo <vika120519841330@gmail.com>
2019-10-01 16:16:59 +0300

    Фотогалерея окончательно доделана, фото откорректированны под один размер

--- RemontUnderKey.InfrastructureServices/Context/MyContext.cs
+++ RemontUnderKey.InfrastructureServices/Context/MyContext.cs
@@ -2160,14 +2160,9 @@ namespace RemontUnderKey.InfrastructureServices.Context
                 ImgSrc = "/Content/1_Minsk_ul_Lojinskaja/78.jpg",
                 ImgSrcMini = "/Content/1_Minsk_ul_Lojinskaja/mini/78.jpg",
                 Repareobject_InfraId = 1
-            };
-            Photo_Infra p79 = new Photo_Infra
-            {
-                Id = 79,
-                ImgSrc = "/Content/1_Minsk_ul_Lojinskaja/79.jpg",
-                ImgSrcMini = "/Content/1_Minsk_ul_Lojinskaja/mini/79.jpg",
-                Repareobject_InfraId = 1
-            };
+            };            
+            
+            //Фото №79 удалено т.к. дубликат
             Photo_Infra p80 = new Photo_Infra
             {
                 Id = 80,
@@ -2203,13 +2198,7 @@ namespace RemontUnderKey.InfrastructureServices.Context
                 ImgSrcMini = "/Content/1_Minsk_ul_Lojinskaja/mini/84.jpg",
                 Repareobject_InfraId = 1
             };
-            Photo_Infra p85 = new Photo_Infra
-            {
-                Id = 85,
-                ImgSrc = "/Content/1_Minsk_ul_Lojinskaja/85.jpg",
-                ImgSrcMini = "/Content/1_Minsk_ul_Lojinskaja/mini/85.jpg",
-                Repareobject_InfraId = 1
-            };
+            //Фото №85 удалено т.к. дубликат
             Photo_Infra p86 = new Photo_Infra
             {
                 Id = 86,
@@ -2294,7 +2283,7 @@ namespace RemontUnderKey.InfrastructureServices.Context
             Photo_Infra p97 = new Photo_Infra
             {
                 Id = 97,
-                ImgSrc = "/Content/2_der_Kopichje_ul_Podgornaya/10.jpg",
+                ImgSrc = "/Content/2_der_Kopichje_ul_Podgornaya/11.jpg",
                 ImgSrcMini = "/Content/2_der_Kopichje_ul_Podgornaya/mini/11.jpg",
                 Repareobject_InfraId = 2
             };
@@ -2655,13 +2644,7 @@ namespace RemontUnderKey.InfrastructureServices.Context
                 ImgSrcMini = "/Content/2_der_Kopichje_ul_Podgornaya/mini/62.jpg",
                 Repareobject_InfraId = 2
             };
-            Photo_Infra p149 = new Photo_Infra
-            {
-                Id = 149,
-                ImgSrc = "/Content/2_der_Kopichje_ul_Podgornaya/63.jpg",
-                ImgSrcMini = "/Content/2_der_Kopichje_ul_Podgornaya/mini/63.jpg",
-                Repareobject_InfraId = 2
-            };
+            //фото №63 и связанная с ним сущность удалена(качество плохое)
             Photo_Infra p150 = new Photo_Infra
             {
                 Id = 150,
@@ -2767,13 +2750,7 @@ namespace RemontUnderKey.InfrastructureServices.Context
                 ImgSrcMini = "/Content/2_der_Kopichje_ul_Podgornaya/mini/78.jpg",
                 Repareobject_InfraId = 2
             };
-            Photo_Infra p165 = new Photo_Infra
-            {
-                Id = 165,
-                ImgSrc = "/Content/2_der_Kopichje_ul_Podgornaya/79.jpg",
-                ImgSrcMini = "/Content/2_der_Kopichje_ul_Podgornaya/mini/79.jpg",
-                Repareobject_InfraId = 2
-            };
+            //фото №79 и связанная с ним сущность удалена(качество плохое)
             Photo_Infra p166 = new Photo_Infra
             {
                 Id = 166,
@@ -2899,13 +2876,9 @@ namespace RemontUnderKey.InfrastructureServices.Context
                 ImgSrcMini = "/Content/3_reg_Minskij_ag_Semkovo/mini/1.jpg",
                 Repareobject_InfraId = 3
             };
-            Photo_Infra p183 = new Photo_Infra
-            {
-                Id = 183,
-                ImgSrc = "/Content/3_reg_Minskij_ag_Semkovo/2.jpg",
-                ImgSrcMini = "/Content/3_reg_Minskij_ag_Semkovo/mini/2.jpg",
-                Repareobject_InfraId = 3
-            };
+
+            //фото №2 и связанная с ним сущность удалена(качество плохое)
+
             Photo_Infra p184 = new Photo_Infra
             {
                 Id = 184,
@@ -3019,13 +2992,13 @@ namespace RemontUnderKey.InfrastructureServices.Context
             modelBuilder.Entity<Photo_Infra>().HasData(p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20,
                                                      p21, p22, p23, p24, p25, p26, p27, p28, p29, p30, p31, p32, p33, p34, p35, p36, p37, p38, p39, p40,
                                                      p41, p42, p43, p44, p45, p46, p47, p48, p49, p50, p51, p52, p53, p54, p55, p56, p57, p58, p59, p60,
-                                                     p61, p62, p63, p64, p65, p67, p68, p69, p70, p71, p72, p73, p74, p75, p76, p77, p78, p79, p80,
-                                                     p81, p82, p83, p84, p85, p86, p87, p88, p89, p90, p91, p92, p93, p94, p95, p96, p97, p98, p99, p100,
+                                                     p61, p62, p63, p64, p65, p67, p68, p69, p70, p71, p72, p73, p74, p75, p76, p77, p78, p80,
+                                                     p81, p82, p83, p84, p86, p87, p88, p89, p90, p91, p92, p93, p94, p95, p96, p97, p98, p99, p100,
                                                      p101, p102, p103, p104, p105, p106, p107, p108, p109, p110, p111, p112, p113, p114, p115, p116, p117, p118, p119, p120,
                                                      p121, p122, p123, p124, p125, p126, p127, p128, p129, p130, p131, p132, p133, p134, p135, p136, p137, p138, p139, p140,
-                                                     p141, p142, p143, p144, p145, p146, p147, p148, p149, p150, p151, p152, p153, p154, p155, p156, p157, p158, p159, p160,
-                                                     p161, p162, p163, p164, p165, p166, p167, p168, p169, p170, p171, p172, p173, p174, p175, p176, p177, p178, p179, p180,
-                                                     p181, p182, p183, p184, p185, p186, p187, p188, p189, p190, p191, p192, p193);
+                                                     p141, p142, p143, p144, p145, p146, p147, p148, p150, p151, p152, p153, p154, p155, p156, p157, p158, p159, p160,
+                                                     p161, p162, p163, p164, p166, p167, p168, p169, p170, p171, p172, p173, p174, p175, p176, p177, p178, p179, p180,
+                                                     p181, p182, p184, p185, p186, p187, p188, p189, p190, p191, p192, p193);
             //modelBuilder.Entity<Photo_mini_Infra>().HasData(mp1, mp2, mp3, mp4, mp5, mp6, mp7, mp8, mp9, mp10, mp11, mp12, mp13, mp14, mp15, mp16, mp17, mp18, mp19, mp20,
             //                                         mp21, mp22, mp23, mp24, mp25, mp26, mp27, mp28, mp29, mp30, mp31, mp32, mp33, mp34, mp35, mp36, mp37, mp38, mp39, mp40,
             //                                         mp41, mp42, mp43, mp44, mp45, mp46, mp47, mp48, mp49, mp50, mp51, mp52, mp53, mp54, mp55, mp56, mp57, mp58, mp59, mp60,
